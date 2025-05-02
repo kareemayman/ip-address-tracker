7488bbad6285c45308164883a3e7f3cde58cf72a
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-05-03 00:34:36 +0300

    chore(start & install dep): start the project & install dependencies

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "IP Address Tracker",
+  description: "Frontend Mentor Challenge",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
